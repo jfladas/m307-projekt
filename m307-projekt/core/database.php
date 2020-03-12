419d8ebbeb8b533cd7c6ba2abeafe8dad8e31248
Lukas <lukas.bucherr@gmail.com>
2020-03-12 10:40:53 +0100

    datenbank

--- m307-projekt/core/database.php
+++ m307-projekt/core/database.php
@@ -6,7 +6,8 @@ function db()
   global $dbConnection;
   if(!$dbConnection){
     try {
-        $dbConnection = new PDO('mysql:host=localhost;dbname=kurseictbz_30713;charset=utf8;', 'kurseictbz_30713', 'db_307_pw_13');
+        $dbConnection = new PDO('mysql:host=127.0.0.1;dbname=fruechte_doerrung;charset=utf8;', 'root', '');
+        //PDO('mysql:host=localhost;dbname=kurseictbz_30713;charset=utf8;', 'kurseictbz_30713', 'db_307_pw_13');
     } catch (PDOException $e) {
         die('Keine Verbindung zur Datenbank möglich: ' . $e->getMessage());
     }
